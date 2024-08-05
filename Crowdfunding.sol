979335d5be71cdbeec03f3152e314cfa17e97e33
terrancrypt <thaiphamngoctuong@gmail.com>
2024-08-05 09:54:52 +0700

    update final code

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -1,59 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 contract Crowdfunding {
-    error NotOwner();
-
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
+    error NoAvailableAmount();
+
+    uint256 public constant MINIMUM_USD = 5e18; // 5 USD in Wei
+    address public immutable i_owner;
+
+    mapping(address => bool) public isFunders;
+    mapping(address => uint256) public funderToAmount;
     address[] public funders;
 
-    address public immutable i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    event Funded(address indexed funder, uint256 value);
+    event Withdrawn(address indexed owner, uint256 value);
 
-    event Funded(address indexed funder, uint256 amount);
-    event Withdrawn(address indexed owner, uint256 amount);
-
-    constructor() {
-        i_owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
+    receive() external payable {
+        fund();
     }
 
     fallback() external payable {
         fund();
     }
 
-    receive() external payable {
-        fund();
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (i_owner != msg.sender) {
+            revert NoAvailableAmount();
+        }
+        _;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "no available amount");
+
+        funderToAmount[msg.sender] += msg.value;
+        bool isFunded = isFunders[msg.sender];
+
+        if (!isFunded) {
+            funders.push(msg.sender);
+            isFunders[msg.sender] = true;
+        }
+
         emit Funded(msg.sender, msg.value);
     }
 
     function withdraw() public onlyOwner {
-        uint256 balance = address(this).balance;
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
+        (bool sent,) = payable(i_owner).call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
 
-        funders = new address[](0);
+        emit Withdrawn(i_owner, address(this).balance);
+    }
 
-        (bool callSuccess,) = payable(msg.sender).call{value: balance}("");
-        require(callSuccess, "Call failed");
-
-        emit Withdrawn(msg.sender, balance);
+    // Tìm ra có bao nhiêu người đã đóng góp
+    function getFundersLength() public view returns (uint256) {
+        return funders.length;
     }
 }
